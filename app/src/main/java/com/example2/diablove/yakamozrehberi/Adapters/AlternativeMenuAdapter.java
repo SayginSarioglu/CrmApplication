b168abc680f3d650737ab5c149c1087a7843432e
SayginSarioglu <e181952@metu.edu.tr>
2017-03-28 14:48:41 +0300

    deneme2

--- app/src/main/java/com/example2/diablove/yakamozrehberi/Adapters/AlternativeMenuAdapter.java
+++ app/src/main/java/com/example2/diablove/yakamozrehberi/Adapters/AlternativeMenuAdapter.java
@@ -17,7 +17,7 @@ import com.example2.diablove.yakamozrehberi.MenuActivities.Toplantilar;
 import com.example2.diablove.yakamozrehberi.R;
 
 /**ksadajskdjaslk
- * ANANI AVRADINI s Created by Diablove on 8/24/2016.
+ *  Created by Diablove on 8/24/2016.
  */
 public class AlternativeMenuAdapter {
 
